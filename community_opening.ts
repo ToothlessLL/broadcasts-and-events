7e1a93ffed06bbef6b2f200e790bab0ef650531f
alan <rubikscubesrs@gmail.com>
2026-06-21 23:40:53 -0700

    maybe final image

--- community_opening.ts
+++ community_opening.ts
@@ -5,9 +5,9 @@ import { CommunityOpening, Colors, ClueTitles, TextOutput, getGPColor } from './
 import { get_skeleton_image } from './skeleton.ts';
 import { Image } from '@napi-rs/canvas';
 
-CommunityOpening.title = '2025 Winter Community Opening';
-CommunityOpening.filename = '2025 Winter Community Opening.png';
-CommunityOpening.width = 1050;
+CommunityOpening.title = '2026 Summer Community Opening';
+CommunityOpening.filename = '2026 Summer Community Opening.png';
+CommunityOpening.width = 1100;
 // CommunityOpening.height += 60;
 const imageRootPath = './images/';
 
@@ -29,57 +29,61 @@ GlobalFonts.registerFromPath(`./Fonts/trajan-pro\\TrajanPro-Regular.ttf`, 'traja
 GlobalFonts.registerFromPath(`.\\Fonts\\runescape_uf\\runescape_uf.ttf`, 'runescape');
 
 /* paste here */
-broadcasts.get(`Barrows dye`).data = {count: 159, value: 123632255};
-broadcasts.get(`Shadow dye`).data = {count: 101, value: 1150000000};
-broadcasts.get(`Ice dye`).data = {count: 89, value: 1050000000};
-broadcasts.get(`Third Age dye`).data = {count: 15, value: 10000000000};
-broadcasts.get(`Blood dye`).data = {count: 18, value: 11000000000};
-broadcasts.get(`Third age full helmet`).data = {count: 2, value: 219268833};
-broadcasts.get(`Third age platebody`).data = {count: 2, value: 449992479};
-broadcasts.get(`Third age platelegs`).data = {count: 4, value: 811032052};
-broadcasts.get(`Third age kiteshield`).data = {count: 0, value: 234190472};
-broadcasts.get(`Backstab cape`).data = {count: 84, value: 2115896};
-broadcasts.get(`Third age mage hat`).data = {count: 2, value: 200041007};
-broadcasts.get(`Third age robe top`).data = {count: 1, value: 223160633};
-broadcasts.get(`Third age robe`).data = {count: 2, value: 202533744};
-broadcasts.get(`Third age amulet`).data = {count: 1, value: 462878166};
-broadcasts.get(`Sack of effigies`).data = {count: 36, value: 2039460};
-broadcasts.get(`Third age ranger coif`).data = {count: 2, value: 198616683};
-broadcasts.get(`Third age ranger body`).data = {count: 0, value: 199172294};
-broadcasts.get(`Third age ranger chaps`).data = {count: 1, value: 196690108};
-broadcasts.get(`Third age vambraces`).data = {count: 3, value: 202412479};
-broadcasts.get(`Explosive barrel`).data = {count: 4, value: 43415769};
-broadcasts.get(`Third age druidic wreath`).data = {count: 5, value: 521002145};
-broadcasts.get(`Third age druidic robe top`).data = {count: 6, value: 229191211};
-broadcasts.get(`Third age druidic robe bottom`).data = {count: 7, value: 194573379};
-broadcasts.get(`Third age druidic staff`).data = {count: 7, value: 190480470};
-broadcasts.get(`Third age druidic cloak`).data = {count: 11, value: 199784979};
+broadcasts.get(`Barrows dye`).data = {count: 219, value: 77765055};
+broadcasts.get(`Shadow dye`).data = {count: 99, value: 800000000};
+broadcasts.get(`Ice dye`).data = {count: 80, value: 750000000};
+broadcasts.get(`Third Age dye`).data = {count: 19, value: 9500000000};
+broadcasts.get(`Blood dye`).data = {count: 22, value: 10000000000};
+broadcasts.get(`Third age full helmet`).data = {count: 9, value: 216809484};
+broadcasts.get(`Third age platebody`).data = {count: 4, value: 402157703};
+broadcasts.get(`Third age platelegs`).data = {count: 5, value: 739197215};
+broadcasts.get(`Third age kiteshield`).data = {count: 4, value: 218770719};
+broadcasts.get(`Backstab cape`).data = {count: 97, value: 1974480};
+broadcasts.get(`Third age mage hat`).data = {count: 4, value: 203797167};
+broadcasts.get(`Third age robe top`).data = {count: 9, value: 205211691};
+broadcasts.get(`Third age robe`).data = {count: 5, value: 199316583};
+broadcasts.get(`Third age amulet`).data = {count: 5, value: 409100362};
+broadcasts.get(`Sack of effigies`).data = {count: 38, value: 2188490};
+broadcasts.get(`Third age ranger coif`).data = {count: 5, value: 199489831};
+broadcasts.get(`Third age ranger body`).data = {count: 4, value: 199857602};
+broadcasts.get(`Third age ranger chaps`).data = {count: 4, value: 200681363};
+broadcasts.get(`Third age vambraces`).data = {count: 3, value: 200749005};
+broadcasts.get(`Explosive barrel`).data = {count: 3, value: 43415769};
+broadcasts.get(`Third age druidic wreath`).data = {count: 8, value: 496572860};
+broadcasts.get(`Third age druidic robe top`).data = {count: 6, value: 220709560};
+broadcasts.get(`Third age druidic robe bottom`).data = {count: 5, value: 200445733};
+broadcasts.get(`Third age druidic staff`).data = {count: 6, value: 197908107};
+broadcasts.get(`Third age druidic cloak`).data = {count: 6, value: 199784979};
 broadcasts.get(`Second-Age full helm`).data = {count: 0, value: 160262136};
-broadcasts.get(`Second-Age platebody`).data = {count: 1, value: 534532453};
+broadcasts.get(`Second-Age platebody`).data = {count: 0, value: 534532453};
 broadcasts.get(`Second-Age platelegs`).data = {count: 0, value: 793611791};
-broadcasts.get(`Second-Age sword`).data = {count: 3, value: 224004485};
-broadcasts.get(`Orlando Smith's hat`).data = {count: 1, value: 16500000000};
+broadcasts.get(`Second-Age sword`).data = {count: 1, value: 224004485};
+broadcasts.get(`Orlando Smith's hat`).data = {count: 2, value: 14500000000};
 broadcasts.get(`Second-Age mage mask`).data = {count: 0, value: 176842716};
-broadcasts.get(`Second-Age robe top`).data = {count: 0, value: 702393889};
-broadcasts.get(`Second-Age robe bottom`).data = {count: 0, value: 719377500};
-broadcasts.get(`Second-Age staff`).data = {count: 1, value: 236889323};
-broadcasts.get(`Golden Compass`).data = {count: 17, value: 0};
-broadcasts.get(`Second-Age range coif`).data = {count: 1, value: 89658487};
-broadcasts.get(`Second-Age range top`).data = {count: 1, value: 254542691};
-broadcasts.get(`Second-Age range legs`).data = {count: 2, value: 270877801};
+broadcasts.get(`Second-Age robe top`).data = {count: 1, value: 702393889};
+broadcasts.get(`Second-Age robe bottom`).data = {count: 1, value: 719377500};
+broadcasts.get(`Second-Age staff`).data = {count: 3, value: 245268304};
+broadcasts.get(`Golden Compass`).data = {count: 9, value: 0};
+broadcasts.get(`Second-Age range coif`).data = {count: 1, value: 93302122};
+broadcasts.get(`Second-Age range top`).data = {count: 1, value: 264245717};
+broadcasts.get(`Second-Age range legs`).data = {count: 1, value: 270877801};
 broadcasts.get(`Second-Age bow`).data = {count: 1, value: 253547630};
 broadcasts.get(`Blank`).data = {count: 0, value: 0};
-const topGP = [{name: 'Jere', gp: 228197417737}, {name: 'Aidenn', gp: 168293544957}, {name: 'Finally', gp: 44610827243}];
-const totalCaskets = 187434;
-const totalParticipants = 77;
-doubleBroadcasts.push({name: 'Jere', item1: 'Barrows dye', item2: 'Sack of effigies'});
-ClueTitles.master.base.list.push('Drashie');
-ClueTitles.hard.base.list.push('Annapoly');
-ClueTitles.elite.golden.list.push({rsn: 'Jere', count: 38140});
-ClueTitles.elite.base.list.push('Thoraa');
-ClueTitles.hard.base.list.push('Ordinaryboyz');
-ClueTitles.elite.golden.list.push({rsn: 'Finally', count: 37285});
-ClueTitles.elite.base.list.push('AgentClue');
+const topGP = [{name: 'Paly', gp: 121461749567}, {name: 'wtf alan', gp: 54007119541}, {name: 'AndyB', gp: 51261062137}];
+const totalCaskets = 225000;
+const totalParticipants = 68;
+// doubleBroadcasts.push({name: '', item1: '', item2: ''});
+// doubleBroadcasts.push({name: '', item1: '', item2: ''});
+ClueTitles.master.golden.list.push({rsn: 'Poptart', count: 20562});
+ClueTitles.hard.base.list.push('Ngis');
+ClueTitles.elite.base.list.push('Keyla');
+ClueTitles.hard.golden.list.push({rsn: 'Elba', count: 37321});
+ClueTitles.master.golden.list.push({rsn: 'Jamten', count: 8577});
+ClueTitles.hard.base.list.push('Sept96');
+ClueTitles.elite.base.list.push('Dio Tha Divi');
+ClueTitles.elite.base.list.push('Ordinaryboyz');
+ClueTitles.master.base.list.push('MaxedSkill');
+ClueTitles.hard.base.list.push('lazy spider');
 /*end here */
 
 const textStart = 490;
@@ -228,51 +232,53 @@ Promise.all([Promise.all(itemImageMap)])
 		textOutput.push({...textConfig});
 	});
 
-	textConfig.text = `Double Broadcasts`;
-	// title = `Double Broadcasts`;
-	textConfig.fillStyle = Colors.ivory;
-	// context.fillStyle = Colors.ivory;
-	currentHeight += context.measureText(textConfig.text).actualBoundingBoxAscent + padding + 10;
-	textConfig.yPosition = currentHeight;
-	textConfig.xPosition = textCenter - context.measureText(textConfig.text).width/2;
-	textOutput.push({...textConfig});
-	// context.fillText(title, textCenter - context.measureText(title).width/2, currentHeight);
-
-	context.fillStyle = Colors.yellow;
-	context.lineWidth = 3;
-	context.strokeStyle = Colors.ivory;
-	context.beginPath();
-	context.moveTo(textCenter - context.measureText(textConfig.text).width/2, currentHeight + 4);
-	context.lineTo(textCenter + context.measureText(textConfig.text).width/2, currentHeight + 4);
-	context.stroke();
-
-	doubleBroadcasts.forEach((value, key) => {
-		let completeString = `${value.name}: ${value.item1} and ${value.item2}`;
-		currentHeight += context.measureText(completeString).actualBoundingBoxAscent + padding;
+	if (doubleBroadcasts.length > 0) {
+		textConfig.text = `Double Broadcasts`;
+		// title = `Double Broadcasts`;
+		textConfig.fillStyle = Colors.ivory;
+		// context.fillStyle = Colors.ivory;
+		currentHeight += context.measureText(textConfig.text).actualBoundingBoxAscent + padding + 10;
 		textConfig.yPosition = currentHeight;
-		textConfig.fillStyle = Colors.yellow;
-		textConfig.xPosition = textCenter - context.measureText(completeString).width/2;
-		textConfig.text = `${value.name}: `;
+		textConfig.xPosition = textCenter - context.measureText(textConfig.text).width/2;
 		textOutput.push({...textConfig});
+		// context.fillText(title, textCenter - context.measureText(title).width/2, currentHeight);
 
-		tempTextConfig.text = textConfig.text;
-		textConfig.text = value.item1;
-		textConfig.fillStyle = broadcasts.get(value.item1).color;
-		textConfig.xPosition = textCenter - context.measureText(completeString).width/2 + context.measureText(tempTextConfig.text).width;
-		textOutput.push({...textConfig});
+		context.fillStyle = Colors.yellow;
+		context.lineWidth = 3;
+		context.strokeStyle = Colors.ivory;
+		context.beginPath();
+		context.moveTo(textCenter - context.measureText(textConfig.text).width/2, currentHeight + 4);
+		context.lineTo(textCenter + context.measureText(textConfig.text).width/2, currentHeight + 4);
+		context.stroke();
 
-		textConfig.fillStyle = Colors.yellow;
-		tempTextConfig.text = `${tempTextConfig.text}${textConfig.text}`;
-		textConfig.text = ` and `;
-		textConfig.xPosition = textCenter - context.measureText(completeString).width/2 + context.measureText(tempTextConfig.text).width;
-		textOutput.push({...textConfig});
+		doubleBroadcasts.forEach((value, key) => {
+			let completeString = `${value.name}: ${value.item1} and ${value.item2}`;
+			currentHeight += context.measureText(completeString).actualBoundingBoxAscent + padding;
+			textConfig.yPosition = currentHeight;
+			textConfig.fillStyle = Colors.yellow;
+			textConfig.xPosition = textCenter - context.measureText(completeString).width/2;
+			textConfig.text = `${value.name}: `;
+			textOutput.push({...textConfig});
 
-		tempTextConfig.text = `${tempTextConfig.text}${textConfig.text}`;
-		textConfig.text = value.item2;
-		textConfig.fillStyle = broadcasts.get(textConfig.text).color;
-		textConfig.xPosition = textCenter - context.measureText(completeString).width/2 + context.measureText(tempTextConfig.text).width;
-		textOutput.push({...textConfig});
-	});
+			tempTextConfig.text = textConfig.text;
+			textConfig.text = value.item1;
+			textConfig.fillStyle = broadcasts.get(value.item1).color;
+			textConfig.xPosition = textCenter - context.measureText(completeString).width/2 + context.measureText(tempTextConfig.text).width;
+			textOutput.push({...textConfig});
+
+			textConfig.fillStyle = Colors.yellow;
+			tempTextConfig.text = `${tempTextConfig.text}${textConfig.text}`;
+			textConfig.text = ` and `;
+			textConfig.xPosition = textCenter - context.measureText(completeString).width/2 + context.measureText(tempTextConfig.text).width;
+			textOutput.push({...textConfig});
+
+			tempTextConfig.text = `${tempTextConfig.text}${textConfig.text}`;
+			textConfig.text = value.item2;
+			textConfig.fillStyle = broadcasts.get(textConfig.text).color;
+			textConfig.xPosition = textCenter - context.measureText(completeString).width/2 + context.measureText(tempTextConfig.text).width;
+			textOutput.push({...textConfig});
+		});
+	}
 
 	textConfig.text = `New Titles`;
 	// title = `New Titles`;
